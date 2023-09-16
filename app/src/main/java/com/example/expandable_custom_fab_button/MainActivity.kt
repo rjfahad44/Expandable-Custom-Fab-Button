0542190eaff3af271c6ce6b9d45145174201567b
Fahad <fahad@hishabee.io>
2023-09-16 10:21:19 +0600

    update

--- app/src/main/java/com/example/expandable_custom_fab_button/MainActivity.kt
+++ app/src/main/java/com/example/expandable_custom_fab_button/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.expandable_custom_fab_button
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.example.expandable_custom_fab_button.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var isExpanded = false
+
+    private val fromBottomFabAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.from_bottom_fab)
+    }
+    private val toBottomFabAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.to_bottom_fab)
+    }
+    private val rotateClockWiseFabAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.rotate_clock_wise)
+    }
+    private val rotateAntiClockWiseFabAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.rotate_anti_clock_wise)
+    }
+    private val fromBottomBgAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim)
+    }
+    private val toBottomBgAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim)
+    }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
