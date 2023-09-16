5b03d7fa12561640b823d693266b79639ea7bc11
Fahad <fahad@hishabee.io>
2023-09-16 09:59:36 +0600

    upload project

---
+++ app/src/main/java/com/example/expandable_custom_fab_button/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.expandable_custom_fab_button
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
